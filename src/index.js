ae81c51ef331fd9e526a25f9cef9d8929c3fb436
Brent Van Minnen <bjvanminnen@gmail.com>
2017-05-28 21:17:01 -0700

    Combine textures

--- src/index.js
+++ src/index.js
@@ -5,11 +5,50 @@ import fragmentShader from './shader.glsl';
 var scene;
 var camera;
 var renderer;
-var uniforms = {};
+
+const texture = new THREE.TextureLoader().load(require('./assets/archer.jpg'));
+texture.wrapS = THREE.ClampToEdgeWrapping;
+texture.wrapT = THREE.ClampToEdgeWrapping;
+texture.minFilter = THREE.NearestFilter;
+
+const alphas = [
+  [0.5, 0.6, 1,   1],
+  [0.6, 1,   1,   1],
+  [1,   1,   1,   1],
+  [1,   0.4, 0.5, 0.6],
+];
+
+var dummyRGBA = new Uint8Array(4 * 4 * 4);
+for (var y = 0; y < 4; y++) {
+  for (var x = 0; x < 4; x++) {
+    const index = 16 * y + 4 * x;
+    dummyRGBA[index] = 255 * alphas[y][x];
+  }
+}
+
+const mapTex = new THREE.DataTexture( dummyRGBA, 4, 4, THREE.RGBAFormat );
+mapTex.needsUpdate = true;
+
+var uniforms = {
+  texture: {
+    type:'t',
+    value: texture
+  },
+  res: {
+    type: 'v2',
+    value: null
+  },
+  map: {
+    type: 't',
+    value: mapTex
+  }
+};
 
 function scene_setup() {
   const width = document.body.scrollWidth;
   const height = document.body.scrollHeight;
+  // uniforms.res.value = new THREE.Vector2(width, height);
+  uniforms.res.value = new THREE.Vector2(612, 380);
   //This is all code needed to set up a basic ThreeJS scene
   //First we initialize the scene and our camera
   scene = new THREE.Scene();
--- src/shader.glsl
+++ src/shader.glsl
@@ -1,3 +1,46 @@
+uniform vec2 res;
+uniform sampler2D texture;
+uniform sampler2D map;
+
 void main() {
-  gl_FragColor = vec4(1, 0, 0, 0);
+  // vec2 m = mod(gl_FragCoord.xy, vec2(4,4)) - vec2(0.5, 0.5);
+  // if (m.x == 0.0) {
+  //   gl_FragColor = vec4(1,0,0,0);
+  // } else {
+  //   gl_FragColor = vec4(1);
+  // }
+
+  vec2 pos = gl_FragCoord.xy / res.xy;
+  if (pos.x > 1.0 || pos.y > 1.0) {
+    gl_FragColor = vec4(0);
+    return;
+  }
+
+  vec2 alphaPos = mod(gl_FragCoord.xy, vec2(4,4)) - vec2(0.5, 0.5);
+  float alpha = texture2D(map, alphaPos).r;
+  gl_FragColor = texture2D(texture, pos) * alpha;
+  // gl_FragColor = texture2D(map, alphaPos);
+
+
+  // float remX = mod(gl_FragCoord.x, 4.0) - 0.5;
+  // float remY = mod(gl_FragCoord.y, 4.0) - 0.5;
+  // float mult = 1.0;
+
+  // float mults[5][5];
+  // mults[0] = vec5(0.6, 0.8, 0.9, 1.0, 1.0);
+  // mults[1] = vec5(0.8, 0.9, 1.0, 0.8, 1.0);
+  // mults[2] = vec5(0.9, 1.0, 0.8, 0.8, 1.0);
+  // mults[3] = vec5(1.0, 0.8, 0.8, 0.6, 1.0);
+  // mults[4] = vec5(1.0, 0.8, 0.8, 0.6, 1.0);
+
+  // for (int row = 0; row < 4; row++) {
+  //   for (int col = 0; col < 4; col++) {
+  //     if (row == int(remY) && col == int(remX)) {
+  //       mult = mults[row][col];
+  //     }
+  //   }
+  // }
+
+
+  // gl_FragColor = mult * gl_FragColor;
 }
